5d074485ee832a46c05f0818e5fa626f42f43f91
lucy-x <lucyclxu@gmail.com>
2022-09-03 00:40:18 -0700

    added code

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+let bg1 = document.getElementById("bg1");
+let text1 = document.getElementById("text1");
 
+
+ window.addEventListener('scroll', function()){
+    var value = window.scroll;
+    bg1.style.top = value + 'px';
+ }
