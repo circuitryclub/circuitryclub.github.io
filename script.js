1b72eb4cd503eecc25aa94887652cf7256afce88
lucy-x <lucyclxu@gmail.com>
2022-09-04 13:39:05 -0700

    added info

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let bg1 = document.getElementById('bg1');
-let text1 = document.getElementById('text1');
+let welcome = document.getElementById('welcome');
 
 
  window.addEventListener('scroll', function(){
     var value = window.scrollY;
 
     bg1.style.top = value * 0.5 + 'px';
-    text1.style.top = value * 0.5 + 'px';
+    welcome.style.top = value * 0.5 + 'px';
  });
